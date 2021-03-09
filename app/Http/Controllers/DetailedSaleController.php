28cc4702e34011e3967abf3ddae7494691f18ebb
darkj18 <50095889+darkj18@users.noreply.github.com>
2021-03-09 02:15:07 -0500

    Se creo los archivos para el crud

---
+++ app/Http/Controllers/DetailedSaleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Detailed_sale;
+use Illuminate\Http\Request;
+
+class DetailedSaleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Detailed_sale  $detailed_sale
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Detailed_sale $detailed_sale)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Detailed_sale  $detailed_sale
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Detailed_sale $detailed_sale)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Detailed_sale  $detailed_sale
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Detailed_sale $detailed_sale)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Detailed_sale  $detailed_sale
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Detailed_sale $detailed_sale)
+    {
+        //
+    }
+}
---
+++ database/factories/DetailedSaleFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Detailed_sale;
+use Faker\Generator as Faker;
+
+$factory->define(Detailed_sale::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
